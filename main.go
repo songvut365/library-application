024f87c7a087eb97850d597c7cf78efb209c4845
songvut365 <songvut.nakrong@gmail.com>
2022-07-20 14:20:17 +0700

    Add cors middleware

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
 	app.Use(logger.New())
+	app.Use(cors.New())
 
 	// Config
 	config.InitDatabase()
